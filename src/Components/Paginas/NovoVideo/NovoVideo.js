b0bf0c8b1267178ab38f46f1ee323fae194477a1
moroni <carlosmoronisud@gmail.com>
2024-07-05 18:05:00 -0300

    aluraflix

---
+++ src/Components/Paginas/NovoVideo/NovoVideo.js
@@ -0,0 +1,21 @@
+
+import Formulario from '../../Formulario';
+
+import './NovoVideo.css';
+
+export default function NovoVideo() {
+
+     
+    
+
+    return (
+        <>
+            <div className='paginaNovoVideo'>
+                <h1>Novo Video</h1>
+                <Formulario />
+                {/* <Formulario Categorias ={categoria.map(categoriasVideos => categoriasVideos.nome)} /> */}
+                
+            </div>
+        </>
+    );  
+}   
